86396a8c5fbba3e20a893a946b5a32aabda5389d
Emad Beyrami <emad.beyrami#>
2021-11-17 21:14:20 +0330

    Initial Commit

---
+++ Bike Citizen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bike Citizen
+//
+//  Created by Emad Bayramy on 11/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
